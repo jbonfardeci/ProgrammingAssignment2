5f55e83f145c8daeab78a94752632bd32452f3a7
John Bonfardeci <john.bonfardeci@gmail.com>
2015-10-24 12:47:28 -0500

    update comments

--- cachematrix.R
+++ cachematrix.R
@@ -34,25 +34,28 @@ cacheSolve <- function(o, ...){
   inv # return the inverse of the matrix
 }
 
+# Unit test for `cacheSolve()`
 assert <- function(){
-  # make a test matrix
+  # make a test matrix 8x8
   x <- (function(n) { 
     i <- 1:n
     1 / outer(i - 1, i, "+") 
   })(8)
   
-  fns <- makeCacheMatrix(x)
+  fns <- makeCacheMatrix(x) # store reference to our getters and setters
   
   cache <- cacheSolve(fns) #set the cache on 1st call
   cached <- cacheSolve(fns) # get cached inverse on 2nd call
   
-  # Test that the 1st node of the inverted matrix is the value we expect, 
-  #  which would be the inverted result of the last node of the original matrix
-  if(isTRUE(all.equal(cached[1,1], 64))){
+  # Test that the 1st and last nodes of the inverted matrix are equal to the 
+  # inverted values of the nodes in the original matrix we expect.
+  if(isTRUE(all.equal(cached[1,1], 64) && all.equal(cached[8,8], 176679358))){
     message("passed")
   } else{
     message("failed")
   }
+  
+  cached
 }
 
 assert() # run the test
